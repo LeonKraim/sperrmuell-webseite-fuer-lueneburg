eccb6881ca63b14eed11911e2f87750f66abb975
Elektroney <off.elektroney@gmail.com>
2026-03-05 21:07:13 +0100

    addded credit

--- components/AppShell.tsx
+++ components/AppShell.tsx
@@ -49,6 +49,17 @@ export default function AppShell() {
             selectedFeature={selectedFeature}
             onPositionChange={setUserPosition}
           />
+          
+          {/* Credit Link */}
+          <a
+            href="https://github.com/LeonKraim"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="absolute bottom-4 left-4 z-[400] rounded-lg bg-white/90 px-3 py-1.5 text-sm font-medium text-gray-700 shadow-md hover:bg-white transition-colors"
+            title="Made by Leon Kraim"
+          >
+            Made by Leon Kraim
+          </a>
         </div>
 
         {/* Right Ad */}
